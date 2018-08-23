1a485e5a535db5a8e86abd59e6c78d49e2c41c42
CohanBonow <cohan.bonow@huskers.unl.edu>
2018-08-23 08:42:17 -0500

    Added my name and major

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 /**
- * Author: Braden Hadwiger
+ * Author: Braden Hadwiger and Cohan Bonow
  * Date: 8/23/2018
  *
- * A simple hello world program in C
+ * A simple program that writes our names and majors in C
  *
  */
 #include<stdlib.h>
@@ -10,7 +10,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("My name is Braden Hadwiger. I am a Mechanical Engineering major.\n");
+  printf("My name is Braden Hadwiger. I am a Mechanical Engineering major.
+          My NAme is Cohan Bonow and I am a computer Science Major\n");
 
   return 0;
 }
