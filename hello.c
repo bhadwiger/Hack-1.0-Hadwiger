2171e8212c3615aecc555dc4d51d27221a09b3e3
bhadwiger <42520214+bhadwiger@users.noreply.github.com>
2018-08-23 08:32:50 -0500

    Code

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Braden Hadwiger
+ * Date: 8/23/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Braden Hadwiger. I am a Mechanical Engineering major.\n");
+
+  return 0;
+}
